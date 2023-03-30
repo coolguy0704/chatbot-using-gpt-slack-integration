2b9e71462bbe08d7183c8f4da95b63217ef26818
Prakash Rao <mb16@iitbbs.ac.in>
2023-03-30 18:20:23 +0900

    modify bucket

--- pipeline/state_backend.tf
+++ pipeline/state_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "chatbot-using-gpt-slack-integration-pipeline-backend"
+    bucket = "chatbot-using-gpt-slack-integration-pipeline-backend-1"
     encrypt = true
     key = "terraform.tfstate"
     region = "us-east-1"
